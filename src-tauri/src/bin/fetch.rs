8d11030be6d40f1dd238351a1e403f44ea5c5c5f
yukihane <yukihane.feather@gmail.com>
2021-10-21 04:50:08 +0900

    データ取得

---
+++ src-tauri/src/bin/fetch.rs
@@ -0,0 +1,6 @@
+use app::init::fetch_remote_data;
+
+fn main() {
+  println!("Hello!");
+  let _ = fetch_remote_data();
+}
--- src-tauri/src/init.rs
+++ src-tauri/src/init.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use easy_scraper::Pattern;
 use std::fs;
 use std::path::Path;
 
@@ -11,6 +12,37 @@ pub fn fetch_data() -> Result<()> {
   }
 
   fs::create_dir_all(data_dir)?;
+
+  Ok(())
+}
+
+pub fn fetch_remote_data() -> anyhow::Result<()> {
+  let content_pattern: Pattern = Pattern::new(
+    r#"
+<div class="card border-0">
+  <a>
+        <img data-original="{{img_src}}">
+  </a>
+    <div class="card-body">
+    <h5 class="card-title mb-2">
+      <a href="{{link}}">{{title}}</a>
+        </h5>
+        <p>
+            <span class="label label-light" style="margin-left:3px">
+              <i class="fab fa-lg fa-fw fa fa-clock"></i>
+              {{time}}</span>
+        </p>
+    <p class="card-text">{{description}}</p>
+  </div>
+</div>
+"#,
+  )
+  .unwrap();
+
+  let text = reqwest::blocking::get("https://novelgame.jp/player/fes")?.text()?;
+  let ms = content_pattern.matches(&text);
+
+  println!("result: {:?}", ms);
 
   Ok(())
 }
